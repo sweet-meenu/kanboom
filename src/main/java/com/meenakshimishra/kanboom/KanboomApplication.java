bc4ce48ed45c8582d4ff130eb9a23817af9d0980
sweet-meenu <meenakshimishra77388@gmail.com>
2026-09-08 00:02:08 +0530

    Initialize repository

---
+++ src/main/java/com/meenakshimishra/kanboom/KanboomApplication.java
@@ -0,0 +1,13 @@
+package com.meenakshimishra.kanboom;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KanboomApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KanboomApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/meenakshimishra/kanboom/KanboomApplicationTests.java
@@ -0,0 +1,13 @@
+package com.meenakshimishra.kanboom;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KanboomApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
